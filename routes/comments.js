2be370d8ef106c7488d7065122f2140c33885500
Angrymouse <rykov.nikas@gmail.com>
2023-07-03 18:24:57 +0300

    add content

--- routes/comments.js
+++ routes/comments.js
@@ -17,6 +17,7 @@ module.exports = fp(async function (app, opts) {
                 return {
                     profile: arprofile,
                     uploaderAddress: comment.address,
+                    content:comment.content||null,
                     timestamp:comment.timestamp,
                     masterAccount: masterAccount || comment.address,
                     id: comment.id,
